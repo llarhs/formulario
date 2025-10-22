c3085adb3f3644783ff927245b768b4f13faf869
Lara <laraleticia8257@gmail.com>
2025-10-22 18:52:21 +0000

    .

--- Delicias da ket/script.js
+++ Delicias da ket/script.js
@@ -1,20 +1,20 @@
-// Espera o documento carregar para rodar o script
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Seleciona todos os botões de pagamento
-    const paymentOptions = document.querySelectorAll('.payment-option');
+    
+    const opcoesPagamento = document.querySelectorAll('.opcao-pagamento');
 
-    // Adiciona um "ouvinte" de clique para cada botão
-    paymentOptions.forEach(option => {
-        option.addEventListener('click', () => {
+    
+    opcoesPagamento.forEach(opcao => {
+        opcao.addEventListener('click', () => {
             
-            // 1. Remove a classe 'selected' de todos os botões
-            paymentOptions.forEach(btn => {
-                btn.classList.remove('selected');
+            
+            opcoesPagamento.forEach(btn => {
+                btn.classList.remove('selecionado');
             });
 
-            // 2. Adiciona a classe 'selected' apenas no botão que foi clicado
-            option.classList.add('selected');
+            
+            opcao.classList.add('selecionado');
         });
     });
 });
